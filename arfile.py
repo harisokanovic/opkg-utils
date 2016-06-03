3ffece9bf19a844edacc563aa092fd1fbfcffeee
Alejandro del Castillo <alejandro.delcastillo@ni.com>
2016-06-03 16:39:52 -0500

    arfile.py: remove dependency on python-future
    Python-future provides a python2 compatible object class (provides next
    method). Since method is not used, remove dependency.
    
    Signed-off-by: Alejandro del Castillo <alejandro.delcastillo@ni.com>

--- arfile.py
+++ arfile.py
@@ -7,7 +7,6 @@ of GNU General Public License v2 or later.
 """
 from __future__ import absolute_import
 from __future__ import print_function
-from builtins import object
 import sys
 import os
 import tarfile 
